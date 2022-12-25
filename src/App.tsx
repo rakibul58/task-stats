473cd6f4e683b2c086477de27e62860b6106525e
rakibul58 <rhrahi14@gmail.com>
2022-12-25 12:40:44 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useColorMode, Button } from '@chakra-ui/react'
+
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+      </Button>
+      <h1>Hello world</h1>
     </div>
   );
 }
